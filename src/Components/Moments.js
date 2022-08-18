c733c6685c497bab9b8fd4aefcf47e385db6e77f
Daniel Jacobson <danieljacobson@Daniels-MacBook-Pro-6.local>
2022-08-18 15:33:45 -0500

    Rendered moments from Supabase

---
+++ src/Components/Moments.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../server/supabaseClient";
+
+const Moments = () => {
+  const [moments, setMoments] = useState([])
+  useEffect(() => {
+    fetchMoments()
+  }, [])
+  
+  async function fetchMoments() {
+    const { data } = await supabase
+      .from('moments')
+      .select()
+    setMoments(data)
+    console.log('data:', data)
+  }
+  
+  return (
+    <div className="App">
+      <div>
+        {moments.map(moment =>(
+          <div key={moment.id}>
+            <p>{moment.content}</p>
+          </div>
+        )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Moments

7f41d618bf487dcc5ce973b847b22d412b1389e7
Daniel Jacobson <danieljacobson@Daniels-MacBook-Pro-6.local>
2022-08-18 15:34:32 -0500

    Applied Prettier

--- src/Components/Moments.js
+++ src/Components/Moments.js
@@ -2,31 +2,28 @@ import { useState, useEffect } from "react";
 import { supabase } from "../server/supabaseClient";
 
 const Moments = () => {
-  const [moments, setMoments] = useState([])
+  const [moments, setMoments] = useState([]);
   useEffect(() => {
-    fetchMoments()
-  }, [])
-  
+    fetchMoments();
+  }, []);
+
   async function fetchMoments() {
-    const { data } = await supabase
-      .from('moments')
-      .select()
-    setMoments(data)
-    console.log('data:', data)
+    const { data } = await supabase.from("moments").select();
+    setMoments(data);
+    console.log("data:", data);
   }
-  
+
   return (
     <div className="App">
       <div>
-        {moments.map(moment =>(
+        {moments.map((moment) => (
           <div key={moment.id}>
             <p>{moment.content}</p>
           </div>
-        )
-        )}
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default Moments
+export default Moments;

46a2c9a284a6040156fd401cd4ddc757a4dad808
efb <evanbarden@gmail.com>
2022-08-18 18:08:56 -0400

    adds styling for inputs and buttons

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { supabase } from "../server/supabaseClient";
+import { Input, InputGroup, Button } from "@chakra-ui/react";
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
 
   const handleLogin = async (email, password) => {
     try {
@@ -32,20 +34,22 @@ export default function Auth() {
     }
   };
 
+  const handleClick = () => setShow(!show);
+
   return (
     <div className="row flex flex-center">
       <div className="col-6 form-widget">
         <h1 className="header">Supabase + React</h1>
         <p className="description">Sign in or sign upyour email below</p>
         <div>
-          <input
+          <Input
             className="inputField"
             type="email"
             placeholder="Your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input
+          <Input
             className="inputField"
             type="password"
             placeholder="Your password"
@@ -54,26 +58,28 @@ export default function Auth() {
           />
         </div>
         <div>
-          <button
+          <Button
             onClick={(e) => {
               e.preventDefault();
               handleLogin(email, password);
             }}
             className={"button block"}
             disabled={loading}
+            size="sm"
           >
             {loading ? <span>Loading</span> : <span>Login</span>}
-          </button>
-          <button
+          </Button>
+          <Button
             onClick={(e) => {
               e.preventDefault();
               handleSignUp(email, password);
             }}
             className={"button block"}
             disabled={loading}
+            size="sm"
           >
             {loading ? <span>Loading</span> : <span>Sign Up</span>}
-          </button>
+          </Button>
         </div>
       </div>
     </div>

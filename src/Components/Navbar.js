56d312f4597e91009211c3bf3222ed7d03dcfb65
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-18 18:11:41 -0400

    removing random line of code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -58,7 +58,6 @@ export default function Navbar() {
                   <MenuItem>testing</MenuItem>
                   <MenuItem>Account Settings (testing)</MenuItem>
                   <MenuItem>Logout (tessting)</MenuItem>
-                  {<MenuItem></MenuItem>}
                 </MenuList>
               </Menu>
             </Box>

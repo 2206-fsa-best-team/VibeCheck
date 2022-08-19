c50b20b03ad5d0f738ae465a47ae631881401856
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-19 15:33:45 -0400

    added routing to the add a moment component

--- src/Components/FloatingAdd.js
+++ src/Components/FloatingAdd.js
@@ -1,28 +1,42 @@
-import { IconButton, Flex, Container, useColorModeValue, Tooltip } from "@chakra-ui/react";
+import {
+  IconButton,
+  Flex,
+  Container,
+  useColorModeValue,
+  Tooltip,
+} from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FloatingAdd = (props) => {
-  const {location} = props
-  console.log(location)
+  const { location } = props;
+  console.log(location);
   return (
     <Container>
       <Flex alignItems="flex-end" direction="row">
-        <Tooltip label={`Add a new ${location}`} placement='left' aria-label={`tooltip for adding a new ${location}`}>
-        <IconButton
-          icon={<AddIcon />}
-          isRound="true"
-          aria-label={`Add a new ${location}`}
-          alignSelf="flex-end"
-          size="lg"
-          bg={useColorModeValue("gray.300", "black")}
-          pos='fixed'
-          px={4}
-          py={4}
-          bottom={24}
-          right={12}
-        />
+      <Link to={`/adda${location}`}>
+        <Tooltip
+          label={`Add a new ${location}`}
+          placement="left"
+          aria-label={`tooltip for adding a new ${location}`}
+        >
+            <IconButton
+              icon={<AddIcon />}
+              isRound="true"
+              aria-label={`Add a new ${location}`}
+              alignSelf="flex-end"
+              size="lg"
+              bg={useColorModeValue("gray.300", "black")}
+              pos="fixed"
+              px={4}
+              py={4}
+              bottom={24}
+              right={12}
+            />
+
         </Tooltip>
+        </Link>
       </Flex>
     </Container>
   );

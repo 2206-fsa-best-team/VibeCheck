84aeb6e6114a6eb9b62164612ba635648f3f81fd
efb <evanbarden@gmail.com>
2022-08-19 16:52:00 -0400

    changes refault app routes and adds welcome profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { supabase } from "./server/supabaseClient";
 import Auth from "./Components/Auth";
 import Moments from "./Components/Moments";
 import Footer from "./Components/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ErrorPage from "./Components/ErrorPage";
 import AddMoment from "./Components/AddMoment";
 
 function App() {
+  const [session, setSession] = useState("");
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        {/* to do: if logged in, take the user to home or display home component, if not, display auth and don't allow any other navigation*/}
-        <Route path="/" element={<Auth />} />
-        <Route path="/moments" element={<Moments />} />
-        <Route path="/add" element={<AddMoment />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <Footer />
+      {!session ? (
+        <Routes>
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            {/* to do: if logged in, take the user to home or display home component, if not, display auth and don't allow any other navigation*/}
+            <Route path="/" element={<Navigate to="/moments" />} />
+            <Route path="/moments" element={<Moments />} />
+            <Route path="/add" element={<AddMoment />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }

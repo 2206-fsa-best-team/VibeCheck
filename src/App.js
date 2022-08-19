0d843bc96e024ec667cdbe19e4eb220c2254b8bc
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-19 11:59:56 -0400

    working on router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Auth from "./Components/Auth";
 import Moments from "./Components/Moments";
+import {
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+  // useLocation,
+  // Redirect,
+  Route,
+} from "react-router-dom";
+import ErrorPage from "./Components/ErrorPage";
+import AddMoment from "./Components/AddMoment";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Auth />
-      <Moments />
+            <Navbar></Navbar>
+    <Routes>
+      {/* to do: if logged in, take the user to home or display home component, if not, display auth */}
+      <Route path="/" element={<Auth />}/>
+      <Route path='/home' element={<Moments/>} />
+      <Route path="/add" element={<AddMoment />}/>
+      <Route path="*" element={<ErrorPage/>}/>
+    </Routes>
     </div>
   );
 }

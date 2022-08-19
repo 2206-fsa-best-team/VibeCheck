4b8e27c4af75489a881c13ac0d0031d7a474a912
danielyj98 <danielyjacobson@gmail.com>
2022-08-19 13:40:01 -0500

    .

--- src/Components/AddMoment.js
+++ src/Components/AddMoment.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "../server/supabaseClient";
 
-const AddMoment=()=> {
+const AddMoment = () => {
+  const [moments, setMoments] = useState([]);
+  const [moment, setMoment] = useState([{ content: "", vibe: 0 }]);
+  const { content, vibe } = moment;
+  useEffect(() => {
+    fetchMoments();
+  }, []);
+
+  async function fetchMoments() {
+    const { data } = await supabase.from("moments").select();
+    setMoments(data);
+  }
+
+  async function createMoment() {
+    await supabase.from("moments").insert([{ content, vibe }]).single();
+    setMoment({ content: "", vibe: 0 });
+    fetchMoments();
+  }
+
   return (
-    <div>hello</div>
-  )
-}
-export default AddMoment
+    <div>
+      <input
+        value={content}
+        onChange={(evt) => setMoment({ ...moment, content: evt.target.value })}
+      />
+      <input
+        value={vibe}
+        onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
+      />
+      <button onClick={createMoment}>Submit</button>
+    </div>
+  );
+};
+export default AddMoment;

26e575f90aea0f82501dae9d0c0213e36279ccd9
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-19 12:15:57 -0400

    updating offline page based on learnings from error page

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,15 +1,18 @@
 import React from "react";
-import {Flex, Heading, Text, Box, Image} from '@chakra-ui/react'
+import {Flex, Heading, Text, Image} from '@chakra-ui/react'
+import { Link } from "react-router-dom";
 
 const ErrorPage=()=> {
   return (
     <Flex alignItems="center" justifyContent='center'>
         <Flex direction='column' background='gray.100' p={12}>
-          <Heading mb={6}>Offline</Heading>
+          <Heading mb={6}>404 Not Found</Heading>
           <Text fontSize='lg' fontWeight='bold'>
-            Looks like you're offline. Please check your internet connection and/or refresh the screen.
+            Looks like we can't find what you're looking for. Please click the Pikachu to head home.
           </Text>
+          <Link to='/home'>
             <Image src='https://www.pngfind.com/pngs/m/299-2991041_memes-para-stickers-png-png-download-surprised-pikachu.png'/>
+            </Link>
           </Flex>
         </Flex>
   )
--- src/Components/Offline.js
+++ src/Components/Offline.js
@@ -1,23 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ChakraProvider, Flex, Heading, Box, Image, Text } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("offline"));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
-      <Flex height="100vh" alignItems="center" justifyContent='center'>
+    <Flex alignItems="center" justifyContent='center'>
         <Flex direction='column' background='gray.100' p={12}>
           <Heading mb={6}>Offline</Heading>
           <Text fontSize='lg' fontWeight='bold'>
-            Looks like you're offline. Please check your internet connection and/or refresh the screen.
+            Looks like you are offline. Try refreshing the page to continue.
           </Text>
-          <Box boxSize='md'>
             <Image src='https://www.pngfind.com/pngs/m/299-2991041_memes-para-stickers-png-png-download-surprised-pikachu.png'/>
-          </Box>
+          </Flex>
         </Flex>
-      </Flex>
     </React.StrictMode>
   </ChakraProvider>
 );

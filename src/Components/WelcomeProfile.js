22cd6da8d1afe360d86f5fb1e41be93abd2eec9b
efb <evanbarden@gmail.com>
2022-08-21 18:22:54 -0400

    user is added to profiles table upon signup email confirmation

--- src/Components/WelcomeProfile.js
+++ src/Components/WelcomeProfile.js
@@ -1,15 +1,31 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { supabase } from "../server/supabaseClient";
+import Moments from "./Moments";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
+  let { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    getProfile();
+    onOpen();
+    getAndSetProfile();
   }, [session]);
 
-  async function getProfile() {
+  async function getAndSetProfile() {
     try {
       setLoading(true);
       const user = supabase.auth.user();
@@ -27,6 +43,8 @@ export default function Account({ session }) {
       if (data) {
         setEmail(data.email);
       }
+
+      updateProfile();
     } catch (error) {
       alert(error.message);
     } finally {
@@ -34,14 +52,15 @@ export default function Account({ session }) {
     }
   }
 
-  async function updateProfile({ email }) {
+  async function updateProfile() {
     try {
       setLoading(true);
       const user = supabase.auth.user();
+      console.log("user-->", user);
 
       const updates = {
         id: user.id,
-        email,
+        email: user.email,
         updated_at: new Date(),
       };
 
@@ -59,37 +78,37 @@ export default function Account({ session }) {
     }
   }
 
+  function closeModal() {
+    onClose();
+    navigate("/moments");
+  }
+
   return (
     <div>
-      <h1>Hello</h1>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="text"
-          value={email || ""}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button
-          className="button block primary"
-          onClick={() => updateProfile({ email })}
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-      </div>
-
-      <div>
-        <button
-          className="button block"
-          onClick={() => supabase.auth.signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
+      <Modal
+        isCentered
+        size="lg"
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        onClick={closeModal}
+      >
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+        <ModalContent>
+          <ModalHeader>welcome to moments</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            we can't wait for you to explore everything our app has to offer.
+            close this popup to create your first moment!
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={closeModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Moments />
     </div>
   );
 }

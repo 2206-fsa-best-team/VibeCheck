ecda2beedd8aad1d3ec43afd24e57701962b48ac
efb <evanbarden@gmail.com>
2022-08-22 15:59:15 -0400

    adds RLS for moments table, refactors AddMoment, and cleans up WelcomeProfile a bit

--- src/Components/AddMoment.js
+++ src/Components/AddMoment.js
@@ -5,14 +5,23 @@ import { useNavigate } from "react-router-dom";
 import MoodSlider from "./Slider";
 
 const AddMoment = () => {
-  const [moment, setMoment] = useState([{ content: "", vibe: "" }]);
+  const [moment, setMoment] = useState({ content: "", vibe: 0 });
   const { content, vibe } = moment;
   let navigate = useNavigate();
+  const user = supabase.auth.user();
 
   async function createMoment() {
-    await supabase.from("moments").insert([{ content, vibe }]).single();
-    setMoment({ content: "", vibe: 0 });
-    navigate("/moments");
+    try {
+      await supabase
+        .from("moments")
+        .insert({ content, vibe, user_id: user.id })
+        .single();
+      setMoment({ content: "", vibe: 0 });
+      navigate("/moments");
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   return (
@@ -22,7 +31,7 @@ const AddMoment = () => {
         onChange={(evt) => setMoment({ ...moment, content: evt.target.value })}
       />
       <input
-        value={vibe || ""}
+        value={vibe || 0}
         onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
       />
       <MoodSlider />

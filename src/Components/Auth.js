58cc42e381fb111d5e4de8474b05f4a7f2ba3c5c
efb <evanbarden@gmail.com>
2022-08-22 18:00:00 -0400

    adds toasts for login stories

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -9,6 +9,7 @@ import {
   HStack,
   Heading,
   InputRightElement,
+  useToast,
 } from "@chakra-ui/react";
 
 export default function Auth() {
@@ -16,15 +17,32 @@ export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
+  const toast = useToast();
 
   const handleLogin = async (email, password) => {
     try {
       setLoading(true);
       const { error } = await supabase.auth.signIn({ email, password });
       if (error) throw error;
-      alert("Sick. You should be logged in now.");
+      toast({
+        title: "logged in",
+        description: "enjoy the moment",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+        position: "top",
+      });
     } catch (error) {
-      alert(error.error_description || error.message);
+      // alert(error.error_description || error.message);
+      console.error(error.error_description || error.message);
+      toast({
+        title: "invalid credentials",
+        description: "please check your email and password and try again.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
     } finally {
       setLoading(false);
     }
@@ -33,7 +51,7 @@ export default function Auth() {
   const handleSignUp = async (email, password) => {
     try {
       setLoading(true);
-      const { error } = await supabase.auth.signUp(
+      const { data, session, error } = await supabase.auth.signUp(
         {
           email,
           password,
@@ -41,8 +59,17 @@ export default function Auth() {
         { redirectTo: "http://localhost:3000/welcome" }
       );
       if (error) throw error;
-
-      alert("Check your email for a verification link.");
+      console.log("data-->", data);
+      console.log("session-->", session);
+      toast({
+        title: "email sent",
+        description:
+          "a verification email has been sent to the address provided",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+        position: "top",
+      });
     } catch (error) {
       alert(error.error_description || error.message);
     } finally {
@@ -50,19 +77,21 @@ export default function Auth() {
     }
   };
 
-  const handleClick = () => setShow(!show);
+  const showPw = () => setShow(!show);
 
   return (
     <Flex>
       <VStack>
         <Heading size="md">Sign in or sign up with your email below</Heading>
         <VStack>
+          {/**** email input ****/}
           <Input
             type="email"
             placeholder="Your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {/**** password input w/ show/hide ****/}
           <InputGroup size="md">
             <Input
               pr="4.5rem"
@@ -72,24 +101,14 @@ export default function Auth() {
               onChange={(e) => setPassword(e.target.value)}
             />
             <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
+              <Button h="1.75rem" size="sm" onClick={showPw}>
                 {show ? "Hide" : "Show"}
               </Button>
             </InputRightElement>
           </InputGroup>
         </VStack>
         <HStack>
-          <Button
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin(email, password);
-            }}
-            disabled={loading}
-            h="1.75rem"
-            size="sm"
-          >
-            {loading ? "Loading" : "Login"}
-          </Button>
+          {/**** signup button ****/}
           <Button
             onClick={(e) => {
               e.preventDefault();
@@ -100,6 +119,18 @@ export default function Auth() {
             size="sm"
           >
             {loading ? "Loading" : "Sign Up"}
+          </Button>
+          {/**** login button ****/}
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin(email, password);
+            }}
+            disabled={loading}
+            h="1.75rem"
+            size="sm"
+          >
+            {loading ? "Loading" : "Login"}
           </Button>
         </HStack>
       </VStack>

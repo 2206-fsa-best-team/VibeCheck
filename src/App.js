daabf9b5d9a1e4ad4f35835dec4973dfea2ed2d6
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-22 14:16:09 -0600

    sidebar added when screens are size lg and up

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import Footer from "./Components/Footer";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ErrorPage from "./Components/ErrorPage";
 import AddMoment from "./Components/AddMoment";
-import { Show } from "@chakra-ui/react";
+import { Box, Show } from "@chakra-ui/react";
 import WelcomeProfile from "./Components/WelcomeProfile";
+import SideMenu from "./Components/SideMenu";
 
 function App() {
   const [session, setSession] = useState("");
@@ -34,19 +35,24 @@ function App() {
       ) : (
         <>
           <Navbar isLoggedIn={isLoggedIn} />
-          <Routes>
-            {/* to do: if logged in, take the user to home or display home component, if not, display auth and don't allow any other navigation*/}
-            <Route path="/" element={<Navigate to="/moments" />} />
-            <Route path="/moments" element={<Moments />} />
-            <Route path="/addamoment" element={<AddMoment />} />
-            <Route
-              path="/welcome"
-              element={
-                <WelcomeProfile key={session.user.id} session={session} />
-              }
-            />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
+          <Show above="lg">
+            <SideMenu />
+          </Show>
+          <Box mt={16} ml={{ base: "0", lg: "20%" }}>
+            <Routes>
+              {/* to do: if logged in, take the user to home or display home component, if not, display auth and don't allow any other navigation*/}
+              <Route path="/" element={<Navigate to="/moments" />} />
+              <Route path="/moments" element={<Moments />} />
+              <Route path="/addamoment" element={<AddMoment />} />
+              <Route
+                path="/welcome"
+                element={
+                  <WelcomeProfile key={session.user.id} session={session} />
+                }
+              />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </Box>
           <Show below="lg">
             <Footer />
           </Show>

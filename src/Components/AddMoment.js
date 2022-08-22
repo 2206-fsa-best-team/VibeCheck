7e61e193bd7908ca8a19f780bae031dacb3811e0
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-22 10:48:26 -0600

    moved slider from App.js to AddMoment.js

--- src/Components/AddMoment.js
+++ src/Components/AddMoment.js
@@ -2,28 +2,30 @@ import React from "react";
 import { useState } from "react";
 import { supabase } from "../server/supabaseClient";
 import { useNavigate } from "react-router-dom";
+import MoodSlider from "./Slider";
 
 const AddMoment = () => {
   const [moment, setMoment] = useState([{ content: "", vibe: "" }]);
   const { content, vibe } = moment;
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   async function createMoment() {
     await supabase.from("moments").insert([{ content, vibe }]).single();
     setMoment({ content: "", vibe: 0 });
-    navigate("/moments")
+    navigate("/moments");
   }
 
   return (
     <div>
       <input
-        value={content || ''}
+        value={content || ""}
         onChange={(evt) => setMoment({ ...moment, content: evt.target.value })}
       />
       <input
-        value={vibe || ''}
+        value={vibe || ""}
         onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
       />
+      <MoodSlider />
       <button onClick={createMoment}>Submit</button>
     </div>
   );

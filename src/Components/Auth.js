3e696b1f2a2eebf7faa93b4614b226a5872f8a9d
efb <evanbarden@gmail.com>
2022-08-23 17:04:30 -0400

    moves toast logic outside click handlers

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -4,6 +4,8 @@ import {
   Input,
   InputGroup,
   Button,
+  ButtonGroup,
+  FormControl,
   Flex,
   VStack,
   HStack,
@@ -20,29 +22,97 @@ export default function Auth() {
   const [show, setShow] = useState(false);
   const toast = useToast();
 
+  // toast alerts for login & signup
+  const ivalidLoginCredentials = () => {
+    toast({
+      title: "invalid credentials",
+      description: "please check your email and password and try again",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const loginSuccess = () => {
+    toast({
+      title: "logged in",
+      description: "enjoy the moment",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const userExists = () => {
+    toast({
+      title: "account already exists",
+      description:
+        "an account for this email address already exists. please log in to that account or request a password reset.",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const verifyEmailSent = () => {
+    toast({
+      title: "email sent",
+      description: "a verification email has been sent to the address provided",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const invalidEmailSignup = () => {
+    toast({
+      title: "invalid email",
+      description: "please enter a valid email and try again.",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const invalidPasswordSignup = (error) => {
+    toast({
+      title: "invalid password",
+      description: `${error.message.toLowerCase()}`,
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const invalidCredentialsSignup = () => {
+    toast({
+      title: "invalid credentials",
+      description:
+        "please use a valid email and password with at least 6 characters",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  // click handlers for login & signup
   const handleLogin = async (email, password) => {
     try {
       setLoading(true);
       const { error } = await supabase.auth.signIn({ email, password });
+      console.log("error", error);
       if (error) throw error;
-      toast({
-        title: "logged in",
-        description: "enjoy the moment",
-        status: "success",
-        duration: 2000,
-        isClosable: true,
-        position: "top",
-      });
+      loginSuccess();
     } catch (error) {
       console.error(error.error_description || error.message);
-      toast({
-        title: "invalid credentials",
-        description: "please check your email and password and try again.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
+      ivalidLoginCredentials();
     } finally {
       setLoading(false);
     }
@@ -59,15 +129,7 @@ export default function Auth() {
         .eq("email", lowercaseEmail);
 
       if (dbCheck.data.length !== 0) {
-        toast({
-          title: "account already exists",
-          description:
-            "an account for this email address already exists. please log in to that account or request a password reset.",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "top",
-        });
+        userExists();
       } else {
         const { error } = await supabase.auth.signUp(
           {
@@ -79,15 +141,7 @@ export default function Auth() {
 
         if (error) throw error;
 
-        toast({
-          title: "email sent",
-          description:
-            "a verification email has been sent to the address provided",
-          status: "success",
-          duration: 2000,
-          isClosable: true,
-          position: "top",
-        });
+        verifyEmailSent();
       }
     } catch (error) {
       console.error(error.error_description || error.message);
@@ -96,35 +150,13 @@ export default function Auth() {
         "Password should be at least 6 characters",
       ];
       if (error.message === commonMsgs[0]) {
-        toast({
-          title: "invalid email",
-          description: "please enter a valid email and try again.",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "top",
-        });
+        invalidEmailSignup();
       } else if (error.message === commonMsgs[1]) {
-        toast({
-          title: "invalid password",
-          description: `${error.message.toLowerCase()}`,
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "top",
-        });
+        invalidPasswordSignup(error);
       }
 
       if (!commonMsgs.includes(error.message)) {
-        toast({
-          title: "invalid credentials",
-          description:
-            "please use a valid email and password with at least 6 characters",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "top",
-        });
+        invalidCredentialsSignup();
       }
     } finally {
       setLoading(false);
@@ -136,42 +168,44 @@ export default function Auth() {
   return (
     <Flex justifyContent="center" alignItems="center" height="50vh">
       <VStack>
-        <Box fontSize={["36px", "48px"]}>moments.</Box>
-        <Heading size="md">get started now</Heading>
-        <VStack>
-          {/**** email input ****/}
-          <Input
-            type="email"
-            placeholder="your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {/**** password input w/ show/hide ****/}
-          <InputGroup size="md">
+        <Box fontSize={["36px", "48px"]}>vibe☑ </Box>
+        <Heading size="md">time to check in</Heading>
+        <FormControl isRequired>
+          <VStack>
+            {/**** email input ****/}
             <Input
-              pr="4.5rem"
-              type={show ? "text" : "password"}
-              placeholder="your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="email"
+              placeholder="your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={showPw}>
-                {show ? "hide" : "show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </VStack>
-        <HStack>
+            {/**** password input w/ show/hide ****/}
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={show ? "text" : "password"}
+                placeholder="your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={showPw}>
+                  {show ? "hide" : "show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </VStack>
+        </FormControl>
+        <ButtonGroup disabled={loading} h="1.75rem" size="sm">
           {/**** signup button ****/}
           <Button
             onClick={(e) => {
               e.preventDefault();
               handleSignUp(email, password);
             }}
-            disabled={loading}
-            h="1.75rem"
-            size="sm"
+            // disabled={loading}
+            // h="1.75rem"
+            // size="sm"
           >
             {loading ? "loading" : "sign up"}
           </Button>
@@ -181,13 +215,13 @@ export default function Auth() {
               e.preventDefault();
               handleLogin(email, password);
             }}
-            disabled={loading}
-            h="1.75rem"
-            size="sm"
+            // disabled={loading}
+            // h="1.75rem"
+            // size="sm"
           >
             {loading ? "loading" : "login"}
           </Button>
-        </HStack>
+        </ButtonGroup>
       </VStack>
     </Flex>
   );

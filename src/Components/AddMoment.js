8e6fe4aac05a80037701727aa4a814a015ad5daa
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-23 11:14:29 -0400

    first commit in the branch

--- src/Components/AddMoment.js
+++ src/Components/AddMoment.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { useState } from "react";
+import React,{ useState, useEffect} from "react";
 import { supabase } from "../server/supabaseClient";
 import { useNavigate } from "react-router-dom";
 import MoodSlider from "./Slider";
+import { Container, VStack, Button, Skeleton } from "@chakra-ui/react";
 
 const AddMoment = () => {
   const [moment, setMoment] = useState({ content: "", vibe: 0 });
   const { content, vibe } = moment;
+  const [pageLoading, setPageLoading] = useState(false);
   let navigate = useNavigate();
   const user = supabase.auth.user();
+  const SliderValue = 50
 
   async function createMoment() {
     try {
@@ -24,19 +26,38 @@ const AddMoment = () => {
     }
   }
 
+  useEffect(() => {
+    setPageLoading(true);
+    console.log(moment)
+    setPageLoading(false);
+  }, [moment]);
+
   return (
-    <div>
-      <input
-        value={content || ""}
-        onChange={(evt) => setMoment({ ...moment, content: evt.target.value })}
-      />
-      <input
-        value={vibe || 0}
-        onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
-      />
-      <MoodSlider />
-      <button onClick={createMoment}>Submit</button>
-    </div>
+    <Container align='center' display={'flex'}bg='blue' pt='36px'>
+      {pageLoading ? (
+        <>
+          <Skeleton h="20px" />
+          <Skeleton h="20px" />
+        </>
+      ) : (
+        <VStack  w='36rem' bg='red'>
+          <input
+            value={content || ""}
+            onChange={(evt) =>
+              setMoment({ ...moment, content: evt.target.value })
+            }
+          />
+          {/* <input
+            value={vibe || 0}
+            onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
+          /> */}
+          <MoodSlider SliderValue={SliderValue} onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })} />
+          <Button onClick={createMoment} >Submit</Button>
+        </VStack>
+      )}
+    </Container>
   );
 };
 export default AddMoment;
+
+// redux for getting the slider state

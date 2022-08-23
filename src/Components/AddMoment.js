fc649eb40c84e8581cf87fb03fe7ef8265e24ce3
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-23 12:11:04 -0400

    adding slider value and working on ui/ux

--- src/Components/AddMoment.js
+++ src/Components/AddMoment.js
@@ -1,8 +1,10 @@
-import React,{ useState, useEffect} from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { supabase } from "../server/supabaseClient";
 import { useNavigate } from "react-router-dom";
 import MoodSlider from "./Slider";
-import { Container, VStack, Button, Skeleton } from "@chakra-ui/react";
+import { Container, VStack, Stack, Button, Skeleton, Textarea, Text } from "@chakra-ui/react";
+
+export const sliderContext = createContext();
 
 const AddMoment = () => {
   const [moment, setMoment] = useState({ content: "", vibe: 0 });
@@ -10,16 +12,20 @@ const AddMoment = () => {
   const [pageLoading, setPageLoading] = useState(false);
   let navigate = useNavigate();
   const user = supabase.auth.user();
-  const SliderValue = 50
+  const [sliderValue, setSliderValue] = useState(50);
 
   async function createMoment() {
     try {
-      await supabase
-        .from("moments")
-        .insert({ content, vibe, user_id: user.id })
-        .single();
-      setMoment({ content: "", vibe: 0 });
-      navigate("/moments");
+      if (!content.length) {
+        alert("Please write a short moment and choose a vibe!");
+      } else {
+        await supabase
+          .from("moments")
+          .insert({ content, vibe: sliderValue, user_id: user.id })
+          .single();
+        setMoment({ content: "", vibe: sliderValue });
+        navigate("/moments");
+      }
     } catch (error) {
       console.error(error);
       throw error;
@@ -28,34 +34,37 @@ const AddMoment = () => {
 
   useEffect(() => {
     setPageLoading(true);
-    console.log(moment)
+    console.log(moment);
     setPageLoading(false);
   }, [moment]);
 
   return (
-    <Container align='center' display={'flex'}bg='blue' pt='36px'>
-      {pageLoading ? (
-        <>
-          <Skeleton h="20px" />
-          <Skeleton h="20px" />
-        </>
-      ) : (
-        <VStack  w='36rem' bg='red'>
-          <input
-            value={content || ""}
-            onChange={(evt) =>
-              setMoment({ ...moment, content: evt.target.value })
-            }
-          />
-          {/* <input
-            value={vibe || 0}
-            onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })}
-          /> */}
-          <MoodSlider SliderValue={SliderValue} onChange={(evt) => setMoment({ ...moment, vibe: evt.target.value })} />
-          <Button onClick={createMoment} >Submit</Button>
-        </VStack>
-      )}
-    </Container>
+    <>
+    <Stack spacing={8}>
+        {pageLoading ? (
+          <>
+            <Skeleton h="20px" />
+            <Skeleton h="20px" />
+          </>
+        ) : (
+          <VStack w="36rem" bg="red">
+            <Text mb='8px'>How is this moment?</Text>
+            <Textarea
+              value={content || ""}
+              onChange={(evt) =>
+                setMoment({ ...moment, content: evt.target.value })
+              }
+            />
+            <MoodSlider
+              sliderValue={sliderValue}
+              setSliderValue={setSliderValue}
+              onChange={(evt) => setMoment({ ...moment, vibe: sliderValue })}
+            />
+            <Button onClick={createMoment}>Submit</Button>
+          </VStack>
+        )}
+        </Stack>
+    </>
   );
 };
 export default AddMoment;

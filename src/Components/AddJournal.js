020978893bc55494a864fb87b86047b3973f9655
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-23 13:29:44 -0600

    final(for now) styling changes on add journal component

--- src/Components/AddJournal.js
+++ src/Components/AddJournal.js
@@ -2,37 +2,58 @@ import React from "react";
 import { useState } from "react";
 import { supabase } from "../server/supabaseClient";
 import { useNavigate } from "react-router-dom";
-import { Button, Input, Textarea } from "@chakra-ui/react";
+import {
+  Button,
+  Input,
+  Spinner,
+  Stack,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import MoodSlider from "./Slider";
 
 const AddJournal = () => {
+  let todayUtc = new Date();
+  const offset = todayUtc.getTimezoneOffset();
+  todayUtc = new Date(todayUtc.getTime() - offset * 60 * 1000);
+  const today = todayUtc.toISOString().split("T")[0];
   const [sliderValue, setSliderValue] = useState(50);
+  const [submitLoading, setSubmitLoading] = useState(false);
   const [journal, setJournal] = useState({
     content: "",
-    date: "",
+    date: today,
   });
-  const { content, vibe, date } = journal;
+  const { content, date } = journal;
   let navigate = useNavigate();
   const user = supabase.auth.user();
-  
+
   async function createJournal() {
+    setSubmitLoading(true);
     try {
-      await supabase
-        .from("journals")
-        .insert({ content, vibe: sliderValue, date, user_id: user.id })
-        .single();
-      setJournal({ content: "", vibe: 0, date: new Date() });
-      // navigate("/journals");
-      console.log(journal);
+      if (!content.length) {
+        alert("Please write a journal and choose a vibe!");
+      } else {
+        await supabase
+          .from("journals")
+          .insert({ content, vibe: sliderValue, date, user_id: user.id })
+          .single();
+        setJournal({ content: "", date: today });
+        navigate("/journals");
+      }
+      setSubmitLoading(false);
     } catch (error) {
       console.error(error);
+      setSubmitLoading(false);
       throw error;
     }
   }
 
   return (
-    <div>
+    <Stack spacing={5} ml="24px" mr="24px" display="flex">
       {/* content input */}
+      <Text mt="32px" ml="8px" fontSize={"24px"}>
+        journalin' time!
+      </Text>
       <Textarea
         value={content}
         onChange={(evt) =>
@@ -40,28 +61,31 @@ const AddJournal = () => {
         }
         resize="vertical"
         placeholder="write your journal here!"
+        size="lg"
       />
       {/* date input */}
+      <Text mt="32px" ml="8px" fontSize={"24px"}>
+        date:
+      </Text>
       <Input
         type="date"
         value={date}
+        max={today}
         onChange={(evt) => setJournal({ ...journal, date: evt.target.value })}
       />
       {/* vibe input */}
-      <MoodSlider
-        sliderValue={sliderValue}
-        setSliderValue={setSliderValue}
-      />
-      <Button
-        onClick={() => {
-          console.log(journal);
-          createJournal();
-        }}
-        colorScheme="teal"
-      >
-        submit
-      </Button>
-    </div>
+      <MoodSlider sliderValue={sliderValue} setSliderValue={setSliderValue} />
+      <div>{/* spacing div */}</div>
+      {submitLoading ? (
+        <Spinner size="md" alignSelf={"center"} colorScheme="tomato" />
+      ) : (
+        <>
+          <Button onClick={() => createJournal()} colorScheme="teal">
+            <Text color="black">submit</Text>
+          </Button>
+        </>
+      )}
+    </Stack>
   );
 };
 

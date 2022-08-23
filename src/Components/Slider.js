45ede8d60434ea24e19546c7e30a69cc424c0e78
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-23 12:50:36 -0600

    add to journal now working!, took out unneeded code from addmoment and slider as well.

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -9,7 +9,7 @@ import { Box, Highlight, Show } from "@chakra-ui/react";
 import { TbFaceId } from "react-icons/tb";
 
 function MoodSlider(props) {
-  const {sliderValue, setSliderValue} = props
+  const { sliderValue, setSliderValue } = props;
 
   const smallLabelStyles = {
     mt: "2",
@@ -41,10 +41,7 @@ function MoodSlider(props) {
       </Highlight>
       <Slider
         aria-label="vibe-check-slider"
-        onChangeEnd={(val) => {
-          setSliderValue(val);
-          console.log(val);
-        }}
+        onChangeEnd={(val) => setSliderValue(val)}
         name="vibe"
         defaultValue={50}
       >

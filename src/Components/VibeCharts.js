b3dd0a1b040d6d5c6a09f5616bc609fd6e174eb4
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-25 17:06:11 -0400

    created filters for the charts

--- src/Components/VibeCharts.js
+++ src/Components/VibeCharts.js
@@ -5,46 +5,93 @@ import VibesLineGraph from "./Charts/VibesLineGraph";
 import { Text } from "@chakra-ui/react";
 import NoDataGraph from "./Charts/NoDataGraph";
 import ChartFilter from "./Charts/ChartFilter";
+import DateObject from "react-date-object";
 
 const VibeCharts = (props) => {
   const [moments, setMoments] = useState([]);
   const [journals, setJournals] = useState([]);
   const [loading, setLoading] = useState(false);
   const [filter, setFilter] = useState("all");
+  const todaysDate = new DateObject();
+  const [filterDate, setFilterDate] = useState("2010-08-05 00:00:00");
 
   useEffect(() => {
     setLoading(true);
+    dateFilter(filter);
     fetchMoments();
     fetchJournals();
     setLoading(false);
-  }, []);
+  }, [filter, filterDate]);
+
+  const dateFilter = async (val) => {
+    let day = todaysDate.day;
+    let month = todaysDate.month.number;
+    let year = todaysDate.year;
+    switch (val) {
+      case "lastSeven":
+        day -= 7;
+        setFilterDate(`${year}-${month}-${day} 00:00:00`);
+        break;
+      case "lastThirty":
+        month -= 1;
+        setFilterDate(`${year}-${month}-${day} 00:00:00`);
+        break;
+      case "lastYear":
+        year -= 1;
+        setFilterDate(`${year}-${month}-${day} 00:00:00`);
+        break;
+      default:
+        year -= 10;
+        setFilterDate(`${year}-${month}-${day} 00:00:00`);
+        break;
+    }
+  };
 
   async function fetchMoments() {
-    const { data } = await supabase.from("moments").select();
-    setMoments(data);
-    console.log(moments);
-    setLoading(false);
+    try {
+      const { data } = await supabase
+        .from("moments")
+        .select()
+        .gt("created_at", `${filterDate}`)
+        .lt("created_at", "2030-08-23 00:00:00")
+        .order("created_at");
+
+      setMoments(data);
+    } catch (err) {
+      console.error(err);
+    }
   }
   async function fetchJournals() {
-    const { data } = await supabase.from("journals").select();
-    setJournals(data);
+    try {
+      const { data } = await supabase
+        .from("journals")
+        .select()
+        // .gt("created_at", `${filterDate}`)
+        .lt("created_at", "2030-08-23 00:00:00")
+        .order("created_at");
+      setJournals(data);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   return (
     <>
       {loading ? (
-        <>
-          <br />
-          <Skeleton height="150px" />
+        <Container w={"90%"}>
           <br />
           <br />
-          <Skeleton height="150px" />
-        </>
+          <Skeleton height="200px" />
+          <br />
+          <br />
+          <br />
+          <Skeleton height="200px" />
+        </Container>
       ) : (
         <>
           <br />
-          {/* <ChartFilter setFilter={setFilter} filter={filter} /> */}
-          {/* <br /> */}
+          <ChartFilter setFilter={setFilter} filter={filter} />
+          <br />
           <Container
             w={"100%"}
             height={"100%"}

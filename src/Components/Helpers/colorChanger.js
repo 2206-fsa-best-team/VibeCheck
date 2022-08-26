e7e50138eff8045471ed2e1d828c8fa936171392
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-26 12:29:17 -0400

    keeping up to date

--- src/Components/Helpers/colorChanger.js
+++ src/Components/Helpers/colorChanger.js
@@ -1,14 +1,14 @@
 export const colorSelector = (vibe) => {
   switch (true) {
-    case vibe <= 20:
-      return "blue.500";
-    case vibe <= 40 && vibe > 20:
-      return "blue.100";
-    case vibe <= 60 && vibe > 40:
-      return "purple.100";
-    case vibe <= 80 && vibe > 60:
-      return "red.300";
     case vibe <= 100 && vibe > 80:
+      return "blue.500";
+    case vibe <= 80 && vibe > 60:
+      return "blue.300";
+    case vibe <= 60 && vibe > 40:
+      return "purple.300";
+    case vibe <= 40 && vibe > 20:
+      return "red.200";
+    case vibe <= 20:
       return "red.600";
     default:
       return "green.600";

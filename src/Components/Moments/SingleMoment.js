caa98ccffb38af14df4e683afef83b9c1c33d8bb
efb <evanbarden@gmail.com>
2022-08-29 15:10:52 -0400

    adds overflowWrap to textarea

--- src/Components/Moments/SingleMoment.js
+++ src/Components/Moments/SingleMoment.js
@@ -32,7 +32,7 @@ const SingleMoment = (props) => {
   let [count, setCount] = useState(0);
   const [loading, setLoading] = useState(true);
   const { momentId } = useParams();
-  console.log(momentId)
+  console.log(momentId);
   useEffect(() => {
     fetchMoment();
   }, []);
@@ -210,6 +210,7 @@ const SingleMoment = (props) => {
                   resize="none"
                   rows={8}
                   maxLength={260}
+                  overflowWrap="word-break"
                   onChange={(e) => setCount(e.target.value.length)}
                 />
                 <EditableControls />

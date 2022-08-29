168724b034fb19852a1f7846f75aec1a4d58a9d8
efb <evanbarden@gmail.com>
2022-08-29 19:20:46 -0400

    moment component broken out into wrapper, card, and details

---
+++ src/Components/Helpers/vibeMsgSelector.js
@@ -0,0 +1,16 @@
+export const vibeMsgSelector = (vibe) => {
+  switch (true) {
+    case vibe <= 20:
+      return "having a tough time";
+    case vibe <= 40 && vibe > 20:
+      return "not at your best";
+    case vibe <= 60 && vibe > 40:
+      return "floating in the middle";
+    case vibe <= 80 && vibe > 60:
+      return "feeling pretty good";
+    case vibe <= 100 && vibe > 80:
+      return "absolutely vibin'";
+    default:
+      return "inconclusive";
+  }
+};

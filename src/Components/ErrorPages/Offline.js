9d96aa86b3d48a03a794cd985feabc20eb095285
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-30 16:49:44 -0400

    fixing offline image

--- src/Components/ErrorPages/Offline.js
+++ src/Components/ErrorPages/Offline.js
@@ -1,21 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ChakraProvider, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Text
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import {TbLetterB, TbLetterE, TbLetterI, TbLetterV} from 'react-icons/tb'
 
 const root = ReactDOM.createRoot(document.getElementById("offline"));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
-    <Flex alignItems="center" justifyContent='center'>
-        <Flex direction='column' background='gray.100' p={12}>
+      <Flex alignItems="center" justifyContent="center">
+        <Flex direction="column" background="gray.100" p={12}>
           <Heading mb={6}>Offline</Heading>
-          <Text fontSize='lg' fontWeight='bold'>
+          <Text fontSize="lg" fontWeight="bold">
             Looks like you are offline. Try refreshing the page to continue.
           </Text>
-            <Image src='https://www.pngfind.com/pngs/m/299-2991041_memes-para-stickers-png-png-download-surprised-pikachu.png'/>
-          </Flex>
+          <Link to="/">
+            <HStack align="center" justify="center">
+              <Icon
+                variant="ghost"
+                color={'tomato'}
+                aria-label="V"
+                w={16}
+                h={16}
+                as={TbLetterV}
+              />
+              <Icon
+                variant="ghost"
+                color={'tomato'}
+                aria-label="V"
+                w={16}
+                h={16}
+                as={TbLetterI}
+              />
+              <Icon
+                variant="ghost"
+                color={'tomato'}
+                aria-label="V"
+                w={16}
+                h={16}
+                as={TbLetterB}
+              />
+              <Icon
+                variant="ghost"
+                color={'tomato'}
+                aria-label="V"
+                w={16}
+                h={16}
+                as={TbLetterE}
+              />
+            </HStack>
+          </Link>
         </Flex>
+      </Flex>
     </React.StrictMode>
   </ChakraProvider>
 );

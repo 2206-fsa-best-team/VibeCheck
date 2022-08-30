ac24a5e8e5d3dc0fb2367dd415f455f2079aa4b7
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-30 10:41:01 -0600

    removed unused var

--- src/Components/Journals/AddJournal.js
+++ src/Components/Journals/AddJournal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { supabase } from "../../server/supabaseClient";
 import { useNavigate } from "react-router-dom";
 import Cam from "../Camera";

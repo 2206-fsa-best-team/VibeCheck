5efa0aabb36ebc51d3a603eced3ec4e4fa330242
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-31 12:37:46 -0400

    added delete toasts for moments and journals

---
+++ src/Components/ToastAlerts/DeleteAlerts.js
@@ -0,0 +1,21 @@
+export const DeletedMoment = () => {
+  return {
+    title: "moment deleted",
+    description: "your moment was successfully deleted",
+    status: "success",
+    duration: 2000,
+    isClosable: true,
+    position: "top",
+  };
+};
+
+export const DeletedJournal = () => {
+  return {
+    title: "journal entry deleted",
+    description: "your journal entry was successfully deleted",
+    status: "success",
+    duration: 2000,
+    isClosable: true,
+    position: "top",
+  };
+};

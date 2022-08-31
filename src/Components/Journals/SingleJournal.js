1e04c0f09b5a72c228c66c3cc5c99dcc93c27363
danielyj98 <105312939+danielyj98@users.noreply.github.com>
2022-08-31 17:27:35 -0400

    Update SingleJournal.js
    Signed-off-by: danielyj98 <105312939+danielyj98@users.noreply.github.com>

--- src/Components/Journals/SingleJournal.js
+++ src/Components/Journals/SingleJournal.js
@@ -49,7 +49,7 @@ const SingleJournal = (props) => {
       let { content, vibe, created_at } = data;
       initializeJournalEntry(content, vibe, created_at);
     } catch (error) {
-      navigate("/error")
+      navigate("/error");
       console.error(error.error_description || error.message);
     } finally {
       setLoading(false);

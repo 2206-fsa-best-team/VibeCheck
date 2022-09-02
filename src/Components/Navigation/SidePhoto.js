0716ef8ca3e809562195d0c39258afd68d2aef37
Daniel Jacobson <danieljacobson@Daniels-MacBook-Pro-6.local>
2022-09-02 11:59:00 -0500

    Side photo goes away for adding moment or journal

--- src/Components/Navigation/SidePhoto.js
+++ src/Components/Navigation/SidePhoto.js
@@ -1,12 +1,26 @@
+import React from "react";
 import { Container, Image, Box } from "@chakra-ui/react";
+const slicedURL = document.location.href.split("/");
+const noSidebar = slicedURL.includes("addamoment" || "addajournal");
 
 const SidePhoto = () => {
   return (
-    <Container>
-      <Box right={0} pos={"fixed"} borderLeft={"1px"} borderColor={"gray.700"}>
-        <Image src="https://i.ibb.co/FqbjLw6/dark-banner-2.png"></Image>
-      </Box>
-    </Container>
+    <>
+      {noSidebar ? (
+        <></>
+      ) : (
+        <Container>
+          <Box
+            right={0}
+            pos={"fixed"}
+            borderLeft={"1px"}
+            borderColor={"gray.700"}
+          >
+            <Image src="https://i.ibb.co/FqbjLw6/dark-banner-2.png"></Image>
+          </Box>
+        </Container>
+      )}
+    </>
   );
 };
 

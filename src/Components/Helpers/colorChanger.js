06400979f37f908bdefc81929e9e7df89ffec7e1
efb <evanbarden@gmail.com>
2022-09-02 12:55:25 -0400

    changes background color of moment card

--- src/Components/Helpers/colorChanger.js
+++ src/Components/Helpers/colorChanger.js
@@ -1,16 +1,16 @@
 export const colorSelector = (vibe) => {
   switch (true) {
     case vibe <= 100 && vibe > 80:
-      return "blue.500";
+      return { border: "blue.600", bg: "blue.500" };
     case vibe <= 80 && vibe > 60:
-      return "blue.300";
+      return { border: "blue.400", bg: "blue.300" };
     case vibe <= 60 && vibe > 40:
-      return "purple.300";
+      return { border: "purple.400", bg: "purple.300" };
     case vibe <= 40 && vibe > 20:
-      return "red.200";
+      return { border: "red.400", bg: "red.300" };
     case vibe <= 20:
-      return "red.600";
+      return { border: "red.600", bg: "red.500" };
     default:
-      return "green.600";
+      return { border: "green.600", bg: "green.500" };
   }
 };

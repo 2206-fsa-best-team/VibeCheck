e08f913b5432ad0680435c8aa555949fd3128f36
danielyj98 <danielyjacobson@gmail.com>
2022-09-04 15:12:02 -0500

    Updated side banners and made them switchable with light and dark modes

--- src/Components/Navigation/SidePhoto.js
+++ src/Components/Navigation/SidePhoto.js
@@ -1,17 +1,11 @@
-import { Show, Image, Box } from "@chakra-ui/react";
+import { Show, Image, Box, useColorModeValue } from "@chakra-ui/react";
 
 const SidePhoto = () => {
+  const photo = useColorModeValue("https://i.ibb.co/Q6JNRKg/light-mode.jpg", "https://i.ibb.co/sQ5S8zy/dark-mode.jpg")
   return (
     <Show above="xl">
-      <Box
-        mt="-16"
-        right={0}
-        zIndex={-2}
-        pos={"fixed"}
-        borderLeft={"1px"}
-        borderColor={"gray.700"}
-      >
-        <Image src="https://i.ibb.co/FqbjLw6/dark-banner-2.png"></Image>
+      <Box mt="-16" right={0} zIndex={-2} w="20%" pos={"fixed"} X>
+        <Image src={photo}></Image>
       </Box>
     </Show>
   );

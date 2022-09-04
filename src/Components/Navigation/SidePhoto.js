f716c313e14fc5ccc9b0d5f58fc1fe3f93d85ff3
danielyj98 <105312939+danielyj98@users.noreply.github.com>
2022-09-04 18:04:52 -0400

    Update src/Components/Navigation/SidePhoto.js
    Great catch- and your solution looks great!
    
    Co-authored-by: Evan Forde Barden <evanbarden@gmail.com>
    Signed-off-by: danielyj98 <105312939+danielyj98@users.noreply.github.com>

--- src/Components/Navigation/SidePhoto.js
+++ src/Components/Navigation/SidePhoto.js
@@ -4,7 +4,13 @@ const SidePhoto = () => {
   const photo = useColorModeValue("https://i.ibb.co/Q6JNRKg/light-mode.jpg", "https://i.ibb.co/sQ5S8zy/dark-mode.jpg")
   return (
     <Show above="xl">
-      <Box mt="-16" right={0} zIndex={-2} w="20%" pos={"fixed"} X>
+      <Box mt="-16" right={0} zIndex={-2} w="20%" pos={"fixed"}  
+      h="100%" 
+      bgSize="100%" 
+      bgImage={`url(${photo})`}
+      backgroundRepeat="repeat"
+      bgPosition="top">
+      X // is this a typo?
         <Image src={photo}></Image>
       </Box>
     </Show>
